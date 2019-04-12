d8a146c6704220ba30ce5e914767fcb3d04b8b23
Vicente Calfo <vicentecalfo@gmail.com>
2019-04-12 16:26:12 -0300

    Commit inicial.

---
+++ src/app/fire-upload/fire-upload.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ez-fire-upload',
+  templateUrl: './fire-upload.component.html',
+  styleUrls: ['./fire-upload.component.scss']
+})
+export class FireUploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/fire-upload/fire-upload.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FireUploadComponent } from './fire-upload.component';
+
+@NgModule({
+  declarations: [FireUploadComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    FireUploadComponent
+  ]
+})
+export class FireUploadModule { }
